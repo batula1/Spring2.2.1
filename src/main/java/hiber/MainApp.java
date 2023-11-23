5d9d3b840725722113b47abaa2de59385a63b2ec
Albert Batula <batula227@gmail.com>
2023-11-23 16:21:20 +0300

    completed

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,10 +1,12 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import javax.persistence.NoResultException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -15,19 +17,37 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
 
+      Car vaz = new Car("Samara", 21014);
+      Car toyota = new Car("Camry", 30);
+      Car volkswagen = new Car("Golf", 7);
+      Car mitsubishi = new Car("Lanser", 9);
+
+      userService.add(user1.setCar(vaz).setUser(user1));
+      userService.add(user2.setCar(toyota).setUser(user2));
+      userService.add(user3.setCar(volkswagen).setUser(user3));
+      userService.add(user4.setCar(mitsubishi).setUser(user4));
+
+
+      // пользователи с авто
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println(user + " "  + user.getCar());
+      }
+      System.out.println("                                                                                           ");
 
+      // по модели авто
+      System.out.println(userService.getUserCar("Lanser", 9));
+      System.out.println("                                                                                           ");
+
+      try {
+         System.out.println(userService.getUserCar("Lenser", 9));
+      }catch (NoResultException e){
+         System.out.println("отсутсвует");
       }
 
       context.close();
--- src/main/java/hiber/service/UserServiceImp.java
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -26,4 +26,10 @@ public class UserServiceImp implements UserService {
       return userDao.listUsers();
    }
 
+   @Override
+   @Transactional(readOnly = true)
+   public User getUserCar(String model, int series) {
+      return userDao.getUserCar(model,series);
+   }
+
 }
